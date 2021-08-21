f9137fae557e414eb72d5d9ecd74dcf651250b04
ianbrdeguzman <ianbrdeguzman@gmail.com>
2021-08-21 07:23:04 -0400

    created types file

---
+++ src/lib/types.ts
@@ -0,0 +1,41 @@
+interface ClientAddress {
+    city: string;
+    country: string;
+    postCode: string;
+    street: string;
+}
+
+interface Items {
+    name: string;
+    price: number;
+    quantity: number;
+    total: number;
+}
+
+interface SenderAddress {
+    city: string;
+    country: string;
+    postCode: string;
+    street: string;
+}
+
+export interface Invoice {
+    clientAddress: ClientAddress;
+    clientEmail: string;
+    clientName: string;
+    createdAt: string;
+    description: string;
+    id: string;
+    items: Items[];
+    paymentDue: string;
+    senderAddress: SenderAddress;
+    status: string;
+    total: number;
+}
+
+export interface InvoiceSlice {
+    loading: boolean;
+    error: string | unknown;
+    invoices: Invoice[];
+    invoice: Invoice | undefined;
+}
